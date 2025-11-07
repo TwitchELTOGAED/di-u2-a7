f6881036bf6c19b56427d5b78e1f838223857a9c
Antonio Jurado <antoniojuradoferreira@gmail.com>
2025-11-07 10:53:03 +0100

    Ejercicio2

--- src/app/Form.js
+++ src/app/Form.js
@@ -1,34 +1,36 @@
+import { useState } from 'react';
+
 export default function Form() {
-    let firstName = '';
-    let lastName = '';
-  
-    function handleFirstNameChange(e) {
-      firstName = e.target.value;
-    }
-  
-    function handleLastNameChange(e) {
-      lastName = e.target.value;
-    }
-  
-    function handleReset() {
-      firstName = '';
-      lastName = '';
-    }
-  
-    return (
-      <form onSubmit={e => e.preventDefault()}>
-        <input
-          placeholder="Nombre"
-          value={firstName}
-          onChange={handleFirstNameChange}
-        />
-        <input
-          placeholder="Apellido"
-          value={lastName}
-          onChange={handleLastNameChange}
-        />
-        <h1>Hola, {firstName} {lastName}</h1>
-        <button onClick={handleReset}>Reiniciar</button>
-      </form>
-    );
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+
+  function handleFirstNameChange(e) {
+    setFirstName(e.target.value);
   }
+
+  function handleLastNameChange(e) {
+    setLastName(e.target.value);
+  }
+
+  function handleReset() {
+    setFirstName('');
+    setLastName('');
+  }
+
+  return (
+    <form onSubmit={e => e.preventDefault()}>
+      <input
+        placeholder="Nombre"
+        value={firstName}
+        onChange={handleFirstNameChange}
+      />
+      <input
+        placeholder="Apellido"
+        value={lastName}
+        onChange={handleLastNameChange}
+      />
+      <h1>Hola, {firstName} {lastName}</h1>
+      <button onClick={handleReset}>Reiniciar</button>
+    </form>
+  );
+}
